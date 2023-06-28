50a62d5456a19c4957a3865492508cf2fb8e334e
sadoming <toraleisha@gmail.com>
2023-06-28 17:55:32 +0200

    V_0.2

--- LIBFT/ft_calloc.c
+++ LIBFT/ft_calloc.c
@@ -6,42 +6,19 @@
 /*   By: sadoming <sadoming@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 17:30:06 by sadoming          #+#    #+#             */
-/*   Updated: 2023/05/24 19:54:20 by sadoming         ###   ########.fr       */
+/*   Updated: 2023/06/28 17:34:41 by sadoming         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_bezero(void *s, size_t n)
-{
-	size_t	cnt;
-	char	*dest;
-
-	dest = (char *) s;
-	cnt = 0;
-	while (cnt < n)
-	{
-		dest[cnt] = '\0';
-		cnt++;
-	}
-}
-
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*call;
-	int		cnt;
 
-	cnt = 0;
-	if (count == 0 && size == 0)
-	{
-		count++;
-		size++;
-	}
-	if (count == 0 || size == 0)
-		return (0);
 	call = malloc(count * size);
 	if (call == 0)
 		return (0);
-	ft_bezero(call, count * size);
+	ft_bzero(call, count * size);
 	return (call);
 }
